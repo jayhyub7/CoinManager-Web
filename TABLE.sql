47dd9dc15e4b9a43f48516de83cd2113c67ce465
jayhyub7 <jayhyub7@gmail.com>
2021-12-27 00:05:59 +0900

    Table Script

--- TABLE.sql
+++ TABLE.sql
@@ -1,19 +1,25 @@
 USE [coin]
 GO
-/****** Object:  Table [dbo].[external_writer]    Script Date: 2021-12-26 오후 6:41:54 ******/
+/****** Object:  Table [dbo].[external_writer]    Script Date: 2021-12-27 오전 12:05:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[external_writer](
-	[member_id] [bigint] NOT NULL,
-	[external_site] [nvarchar](20) NOT NULL,
+	[member_email] [nvarchar](50) NOT NULL,
+	[external_site_code] [nvarchar](20) NOT NULL,
 	[nick_name] [nvarchar](50) NOT NULL,
 	[ins_date] [datetime] NULL,
-	[upd_date] [datetime] NULL
+	[upd_date] [datetime] NULL,
+ CONSTRAINT [PK_external_writer] PRIMARY KEY CLUSTERED
+(
+	[member_email] ASC,
+	[external_site_code] ASC,
+	[nick_name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[member]    Script Date: 2021-12-26 오후 6:41:54 ******/
+/****** Object:  Table [dbo].[member]    Script Date: 2021-12-27 오전 12:05:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
